25d619300944d619cbb699b957e78721f0552859
Rafael Antonio Gomez Casas <dotzecker@gmail.com>
2013-10-03 22:28:40 +0200

    Base, sin mucho más

---
+++ src/DotZecker/Larafeed/Item.php
@@ -0,0 +1,9 @@
+<?php namespace DotZecker\Larafeed;
+
+class Item {
+
+
+    public function __construct()
+    {
+    }
+}
---
+++ src/DotZecker/Larafeed/Larafeed.php
@@ -0,0 +1,12 @@
+<?php namespace DotZecker\Larafeed;
+
+class Larafeed {
+
+
+    public function make()
+    {
+        return new Larafeed();
+    }
+
+
+}
