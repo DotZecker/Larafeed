5645acf287358eec302fb060188df3b1874f1bcd
Rafael Antonio Gomez Casas <dotzecker@gmail.com>
2013-10-03 22:44:12 +0200

    Atom and RSS views from Roumen\Feed

---
+++ src/views/atom.blade.php
@@ -0,0 +1,31 @@
+{{ '<?xml version="1.0" encoding="UTF-8" ?>'."\n" }}
+<feed xmlns="http://www.w3.org/2005/Atom">
+    <title type="html">{{ $channel['title'] }}</title>
+    <subtitle type="html">{{ $channel['description'] }}</subtitle>
+    <link href="{{ $channel['link'] }}"></link>
+    <id>{{ $channel['link'] }}</id>
+    <link rel="alternate" type="text/html" href="{{ $channel['link'] }}" ></link>
+    <link rel="self" type="application/atom+xml" href="{{ Request::url() }}" ></link>
+
+    @if ( ! empty($channel['logo']))
+        <logo>{{ $channel['logo'] }}</logo>
+    @endif
+
+    @if ( ! empty($channel['icon']))
+        <icon>{{ $channel['icon'] }}</icon>
+    @endif
+
+    <updated>{{ date('c', strtotime($channel['pubdate'])) }}</updated>
+    @foreach($items as $item)
+        <entry>
+            <author>
+                <name>{{ $item['author'] }}</name>
+            </author>
+            <title type="html"><![CDATA[{{ $item['title'] }}]]></title>
+            <link rel="alternate" type="text/html" href="{{ $item['link'] }}"></link>
+            <id>{{ $item['link'] }}</id>
+            <summary type="html"><![CDATA[{{ $item['description'] }}]]></summary>
+            <updated>{{ date('c', strtotime($item['pubdate'])) }}</updated>
+        </entry>
+    @endforeach
+</feed>
